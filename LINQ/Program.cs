747d809102b0694ac09a27d76ac5e71ca2899e49
Jayson Hundsrucker <hundsruckerjayson@gmail.com>
2024-03-07 08:58:53 -0800

    Completed Assignment

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>
+        {
+            "Rust",
+            "BlackOps 2",
+            "Elden Ring",
+            "Destiny 2",
+            "Dark Souls III",
+            "Fallout 76",
+            "60 seconds",
+            "Bloodborne",
+            "Uncharted 4: A Thief's End"
+        };
+
+            var orderedGames = videoGames.OrderBy(game => game.Length);
+
+            Console.WriteLine("Ordered list of video games by length:");
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
